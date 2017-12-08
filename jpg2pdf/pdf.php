7ce5887293ff5dc66e97ac61914543aa2d6d3bef
charles66820 <30560349+charles66820@users.noreply.github.com>
2017-12-08 17:35:56 +0100

    Add files via upload

--- jpg2pdf/pdf.php
+++
@@ -1,8 +0,0 @@
-$img = new Imagick('image.jpg');
-$img = new Imagick('image1.jpg');
-
-$img->setImageFormat("pdf");
-$img->writeImage("file.pdf[0]");
-
-$img1->setImageFormat("pdf");
-$img1->writeImage("file.pdf[1]");
