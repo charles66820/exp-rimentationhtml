fe81eac5dacd91fba643b5bd1dfa55e2c245cf22
charles66820 <charles.goedefroit@gmail.com>
2019-01-25 17:04:56 +0100

    buffer test

--- server/php/chunkResContent.php
+++ server/php/chunkResContent.php
@@ -1,7 +1,4 @@
 <?php
-ignore_user_abort(true); //sésactive la mort du script si le client quite la page
-set_time_limit(0); //désactive le time out
-
 header('Content-Type: text/plain'); //done le type du contenu de la requet
 header('Connection: Keep-Alive');
 header('X-Content-Type-Options: nosniff'); //permet d'envoyer les donner par chunk
@@ -9,9 +6,9 @@ header('X-Content-Type-Options: nosniff'); //permet d'envoyer les donner par chu
 $i = 1000;
 while ($i-- > 0) {
   ob_end_flush();
-  file_put_contents('/dev/stdout', connection_status());
-  //ob_flush();
-  flush();
+  //file_put_contents('/dev/stdout', connection_status()); //écrit dans la console
+  echo "msg\n";
+  //flush();
   ob_start();
   usleep(25000);
 }
